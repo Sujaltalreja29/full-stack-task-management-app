9e6d9d5e1d5f58b7996060d940e12473945743d9
Sujaltalreja29 <sujaltlrj@gmail.com>
2025-02-01 13:53:09 +0530

    added jwt to landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Auth/Login';
@@ -11,49 +10,56 @@ import OrderPage from './pages/Order';
 import OrderHistory from './pages/History';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-function App() {
 
+function HomeRedirect() {
+  const { user } = useAuth();  // Fetch user authentication status
+  return user ? <Navigate to="/menu" /> : <Home />;
+}
+
+function App() {
   return (
     <Router>
       <AuthProvider>
-      <CartProvider>
-        <Navbar />
+        <CartProvider>
+          <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* Redirect logged-in users from Home to Menu */}
+            <Route path="/" element={<HomeRedirect />} />
+
+            {/* Login and Register remain accessible */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route
-            path="/menu"
-            element={
-              <ProtectedRoute>
-                <Menu />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/menu" />} />
-          <Route
-            path="/order"
-            element={
-              <ProtectedRoute>
-                < OrderPage/>
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/order"/>} />
-          <Route
-            path="/history"
-            element={
-              <ProtectedRoute>
-                < OrderHistory/>
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/history"/>} />
+
+            {/* Protected Routes (Require Authentication) */}
+            <Route 
+              path="/menu" 
+              element={
+                <ProtectedRoute>
+                  <Menu />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/order" 
+              element={
+                <ProtectedRoute>
+                  <OrderPage />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/history" 
+              element={
+                <ProtectedRoute>
+                  <OrderHistory />
+                </ProtectedRoute>
+              } 
+            />
           </Routes>
-          </CartProvider>
+        </CartProvider>
       </AuthProvider>
-      </Router>
-    );
+    </Router>
+  );
 }
 
-export default App
+export default App;
